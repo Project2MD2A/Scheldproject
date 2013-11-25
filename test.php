6a409b5e4f916043b41ef40532a72d1a8a84092a
Meesverberne <meesverberne@Live.nl>
2013-11-25 11:14:52 +0100

    Veranderingen database
    Veranderingen ophalen database + Vote

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
-//Deze functie is gemaakt door: Orin
-
+// deze functie is geschreven door Orin
+// aangepast door Orin & Mees
 error_reporting('E_ALL');
 // leg connectie met de database : server username wachtwoord database.
 $link = mysqli_connect("localhost", "admin_project", "sintlucas123", "admin_project") or die(mysqli_error());
@@ -10,17 +10,22 @@ if (mysqli_connect_errno()) {
     exit();
 }
 
-// de query die je gaat toepassen op de database.
-$sql = "SELECT id, tweet, time, user FROM tbl_tweets ORDER BY id DESC";
 
-// indien er een resultaat uitkomt doe dan.
+
+// de query die je gaat toepassen op de database.
+$sql = "SELECT id, tweet, time, user, vote FROM tbl_tweets ORDER BY id DESC";
+
+
+// indien er een esultaat uitkomt doe dan.
 $result = mysqli_query($link, $sql);
 
 
-$htmlOutput ="<table><tr><th>id</th><th>tweet</th><th>time</th><th>user</th></tr>";
+//print_r($result);
+
+$htmlOutput ="<table><tr><th>id</th><th>tweet</th><th>time</th><th>user</th><th>vote</th></tr>";
 while($row = mysqli_fetch_array($result)){
 	// laat de inoud van de tabel zien.
-	$htmlOutput .= "<tr><td>".$row["id"]."</td><td>".$row["tweet"]."</td><td>".$row["time"]."</td><td>".$row["user"]."</td></tr>";
+	$htmlOutput .= "<tr><td>".$row["id"]."</td><td>".$row["tweet"]."</td><td>".$row["time"]."</td><td>".$row["user"]."</td><td>".$row["vote"]."</td></tr>";
 }
 $htmlOutput .= "</tabe>";
 
@@ -43,5 +48,6 @@ th {
 
 <body>
 	<?php echo($htmlOutput); ?>
+
 </body>
 </html>
